66434d91a0f2ca1488a3c141a9cd6785f45b1804
Anton Kosyakin <anton@tinuviel.ru>
2015-04-14 17:33:01 +0300

    simple time complexity measurement for 3-Sum

--- src/test/java/com/github/java_experiments/junit/ThreeSumTest.java
+++ src/test/java/com/github/java_experiments/junit/ThreeSumTest.java
@@ -17,5 +17,38 @@ public class ThreeSumTest {
         assertThat(alg.count(a), equalTo(4));
     }
 
+    @Test
+    public void checkTimeComplexity() {
+        int n = 100;
+        long firstDiff = 0;
+        for (int i = 0; i < 5; i++) {
+            int[] data = generateInputs(n << i); // n, 2*n, 4*n
+
+            long start = System.currentTimeMillis();
+            new ThreeSumBruteForceImpl().count(data);
+            long diff = System.currentTimeMillis() - start;
+
+            if (firstDiff == 0) {
+                System.out.print(String.format("%d ms", diff));
+                firstDiff = diff;
+            } else {
+                System.out.print(String.format("  %.1f", diff / (double) firstDiff));
+                System.out.flush();
+            }
+        }
+
+        System.out.println();
+    }
+
     private static final int[] a = {30, -40, -20, -10, 40, 0, 10, 5};
+
+    private int[] generateInputs(int n) {
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            result[i] = (int) (Math.random() * n * 2) - n;
+        }
+
+        return result;
+    }
 }
